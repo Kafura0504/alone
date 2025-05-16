ec4120210247073281fdf3b285c6d2b895ae5bce
Kafura <robbykafura@gmail.com>
2025-05-16 07:16:59 +0700

    3

---
+++ alone/Assets/sekrip/anomalyController.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anomalyController : MonoBehaviour
+{
+    public GameObject[] _anomaly;
+    public enum playtime
+    {
+        minute5,
+        minute6
+    }
+    public playtime settime;
+    private float timer;
+    private List<GameObject> _inactive;
+    private float anomalytimer;
+
+    void Awake()
+    {
+        for(int i= 0 ; i<_anomaly.Length;i++)
+        {
+            _anomaly[i].SetActive(false);
+        }
+        _inactive = new List<GameObject>(_anomaly);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (settime==playtime.minute5)
+        {
+            timer = 300f;
+        } else if(settime==playtime.minute6)
+        {
+            timer = 360f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            anomalytimer -= Time.deltaTime;
+
+            if (anomalytimer <= 0)
+            {
+                spawnanomaly();
+                anomalyreset();
+            }
+        }
+    }
+
+    public int getindex()
+    {
+        int index;
+        index = UnityEngine.Random.Range(0,_anomaly.Length);
+        return index;
+    }
+
+    void anomalyreset()
+{
+    if (timer >= 240)
+    {
+        anomalytimer = 45;
+    }
+    else if (timer >= 180)
+    {
+        anomalytimer = 30;
+    }
+    else if (timer >= 120)
+    {
+        anomalytimer = 25; 
+    }
+    else if (timer >= 60)
+    {
+        anomalytimer = 20;
+    }
+    else
+    {
+        anomalytimer = 15; // fallback for final minute
+    }
+}
+
+    public void spawnanomaly()
+    {
+
+    if (_anomaly.Length > 0)
+    {
+        int index = getindex();
+        if (_anomaly[index] != null && _inactive[index] != null) // to make sure that no same anomaly's called
+        {
+        _inactive[index].SetActive(true);
+        _inactive[index] = null; // i want to delete the data from this array so it will return as null next time it's called
+        }
+    }
+
+    }
+}
